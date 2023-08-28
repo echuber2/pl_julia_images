68215b300ec93aa8dd160f855da517f70d2e74e4
Christopher Tessum <ctessum@gmail.com>
2023-08-28 15:55:23 -0500

    Fix problem with loading Plots package

--- build_all.sh
+++ build_all.sh
@@ -4,7 +4,7 @@
 #export platform="linux/arm64/v8"
 export platform="linux/amd64" # Cross-compilation not working.
 
-tag="0.4.0"
+tag="0.4.1"
 
 
 #for image in pl-julia-datascience-pluto-workspace
@@ -18,9 +18,10 @@ done
 
 # 0.2.0 time to working HW1: 12:41 (laggy even after this)
 # 0.3.0 time to working HW1: 6:41
+# 0.4.0 time to working HW1: 3:45
 
 # gcloud 
 # 0.2.0: 10s startup, 3.4GB memory
 # 0.3.0: 18s startup, 3.4GB memory
 # 0.3.1: 13s startup, 3.4GB memory
-# 0.4.0: 16s startup, 1.0GB memory?
+# 0.4.0: 16s startup, ~600MB memory?
